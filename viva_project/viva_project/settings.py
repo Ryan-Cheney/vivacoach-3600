1eb993feb126f1d8e810bd2df3a3268990db4905
Ryan Cheney <143455863+Ryan-Cheney@users.noreply.github.com>
2024-04-25 23:04:44 -0600

    RDS added

--- viva_project/viva_project/settings.py
+++ viva_project/viva_project/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import pymysql
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,8 +82,12 @@ WSGI_APPLICATION = "viva_project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "vivacoach",
+        "USER": "admin",
+        "PASSWORD": "vivacoach64!",
+        "HOST": "vivacoach-db.ch8gios02gk5.us-west-1.rds.amazonaws.com",
+        "PORT": "3306",
     }
 }
 
